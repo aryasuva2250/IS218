ff3b21db4421b98eb4cb496c9e6d70ed68af438c
aryasuva2250 <as3526@njit.edu>
2021-06-11 19:07:14 -0400

    added python script

---
+++ my_script.py
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+exec vim "$@"
+
+# Sample taken from pyStrich GitHub repository
+# https://github.com/mmulqueen/pyStrich
+from pystrich.datamatrix import DataMatrixEncoder
+
+encoder = DataMatrixEncoder('This is a DataMatrix.')
+encoder.save('./datamatrix_test.png')
+print(encoder.get_ascii())
